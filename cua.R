dc10d5222a09a6835fbdcc7ac3d8328bb37b1e2c
Psychophilours <65535025+Psychophilours@users.noreply.github.com>
2021-05-11 12:44:54 +0200

    anal
    qqs analyses

--- cua.R
+++ cua.R
@@ -1,8 +1,11 @@
-# Load library bite
+# Load library 
 library(limer)
 library(tidyverse)
 library(lubridate)
 library(rio)
+#install.packages("devtools")
+#devtools::install_github("andrewheiss/limer")
+
 
 # Setup API details
 options(lime_api = 'https://survey.competences-emotionnelles.ch/admin/remotecontrol')
@@ -36,3 +39,70 @@ d$dat <- ymd_hms(d$dat)
 
 # Sortie Excel si souhaité
 export(d,"sortie.xlsx")
+
+
+
+
+### Premières analyses
+
+#load library
+
+library(psych)
+
+
+# first descriptions
+
+
+summary(d)
+head(d)
+describe(d)
+names(d)
+
+
+# recode variables
+d$pro <- as.factor(d$pro)
+d$sex <- as.factor(d$sex)
+d$can <- as.factor(d$can)
+
+summary(d)
+
+rep.df <- d[9:28]
+clar.df <- d[29:48]
+
+describe(rep.df)
+describe(clar.df)
+
+boxplot(rep.df, las=2, notch = T)
+boxplot(clar.df, las=2, notch = T)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### KO
+## describe likert
+#Covert values to factors
+library(likert)
+rep.df= factor(rep.df, levels = c("1", "2", "3", "4"), ordered = TRUE)
+
+likert(rep.df)
+
+rep.df <- lapply(d, factor, levels = 1:4)
+
+# Create a likert object
+ex_1_likert = likert(d)
+
+# Figure 2
+
+plot(d, ordered = FALSE, group.order = names(ex_1[2:5]))
+
+
